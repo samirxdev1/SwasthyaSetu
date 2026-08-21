57f686e3f9e639024e19c9717138f3897b542377
Samir Mansuri <mansurisamir493@gmail.com>
2026-08-21 10:26:52 +0530

    Vercel Web Analytics Implimented

--- swasthyasetu-web/src/App.jsx
+++ swasthyasetu-web/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import { AuthProvider } from './context/AuthContext';
+import { Analytics } from '@vercel/analytics/react';
+
+
 
 function App() {
   return (
